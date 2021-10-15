d0c9e7fec7b99ec8efc432cc598125ac893e00c4
gemamm <gemamm1988@gmail.com>
2021-10-15 12:21:06 +0200

    act

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,6 +8,9 @@ public class Test {
     public static void main(String[] args) {
         Generica<Integer> objInt = new Generica(15);
         objInt.obtenerTipo();
+        
+        Generica<String> objstring = new Generica("Gema");
+        objstring.obtenerTipo();
     }
     
 }
