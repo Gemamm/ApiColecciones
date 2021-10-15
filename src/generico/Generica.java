5714d116f4e8b90e6a89238f12ab94e3023ecc67
gemamm <gemamm1988@gmail.com>
2021-10-15 12:18:59 +0200

    act

--- src/generico/Generica.java
+++ src/generico/Generica.java
@@ -10,5 +10,8 @@ public class Generica <T>{
         this.objeto = objeto;
     }
     
-    
+    public void obtenerTipo(){
+        System.out.println("El tipo T es: " +objeto.getClass().getSimpleName());
+        
+    }    
 }
---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+
+package test;
+
+import generico.Generica;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Generica<Integer> objInt = new Generica(15);
+        objInt.obtenerTipo();
+    }
+    
+}
